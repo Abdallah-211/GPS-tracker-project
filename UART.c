9ee2e909979e8965536c93b2337d4f8386915d50
Mohamed23 <133220811+Mazed24@users.noreply.github.com>
2024-05-02 13:13:28 +0300

    Adding UART57

--- UART.c
+++ UART.c
@@ -112,3 +112,105 @@ void UART1_ReadString(char *str , int len)	      	//Read String
             str++;
         }
     }
+//*****************************************************************************
+//
+// UART5
+//
+//*****************************************************************************
+
+void UART5_Init(){
+    SYSCTL_RCGCUART_R |= 0x0020;
+    SYSCTL_RCGCGPIO_R |= 0X10;
+	while((SYSCTL_PRGPIO_R & (0x01<<4)) == 0){}
+  UART5_CTL_R =~UART_CTL_UARTEN;
+	UART5_IBRD_R = 104;
+	UART5_FBRD_R = 11;
+	UART5_LCRH_R |= 0x70;
+	UART5_CTL_R |= 0x301;
+	GPIO_PORTE_DEN_R |= 0x30;
+	GPIO_PORTE_AFSEL_R |= 0x30;
+	GPIO_PORTE_AMSEL_R &= ~0x30;
+	GPIO_PORTE_PCTL_R |= (GPIO_PORTE_PCTL_R&~0XFF)|(0X11);
+	
+}
+void UART5_WriteChar(unsigned char ch){						//Write Char
+	while((UART5_FR_R&0x20) != 0){}
+	UART5_DR_R = ch;
+}
+
+unsigned char UART5_ReadChar(void){								//Read Char
+	while((UART5_FR_R&0x10) != 0){}
+	return UART5_DR_R&0xFF;
+}
+
+void UART5_ReadString(char *str , int len)	      	//Read String
+{	
+ char c = UART5_ReadChar();
+	int i;
+	for(i=0;i<len;i++){
+		if(c!=CR){
+		*str = c;
+		str++;
+		UART5_WriteChar(c);
+		}
+		else if(c==CR || i==len) break;
+	}
+}
+ void printStr5(char *str){       //Print String
+        while(*str){
+            UART5_WriteChar(*str);
+            str++;
+        }
+    }
+
+//*****************************************************************************
+//
+// UART7
+//
+//*****************************************************************************
+
+void UART7_Init(){
+	
+	SYSCTL_RCGCUART_R |= 0x0080;
+    SYSCTL_RCGCGPIO_R |= 0X0010;
+	while((SYSCTL_PRGPIO_R & (0x01<<4)) == 0){}
+	UART7_CTL_R =~UART_CTL_UARTEN;
+	UART7_IBRD_R = 104;
+	UART7_FBRD_R = 11;
+	UART7_LCRH_R |= 0x70;
+	UART7_CTL_R |= 0x301;
+	GPIO_PORTE_DEN_R |= 0x03;
+	GPIO_PORTE_AFSEL_R |= 0x03;
+	GPIO_PORTE_AMSEL_R &= ~0x03;
+	GPIO_PORTE_PCTL_R |= (GPIO_PORTE_PCTL_R&~0XFF)|(0X11);
+	
+}
+void UART7_WriteChar(unsigned char ch){						//Write Char
+	while((UART7_FR_R&0x02) != 0){}
+	UART7_DR_R = ch;
+}
+
+unsigned char UART7_ReadChar(void){								//Read Char
+	while((UART7_FR_R&0x01) != 0){}
+	return UART7_DR_R&0xFF;
+}
+
+void UART7_ReadString(char *str , int len)	      	//Read String
+{	
+ char c = UART7_ReadChar();
+	int i;
+	for(i=0;i<len;i++){
+		if(c!=CR){
+		*str = c;
+		str++;
+		UART7_WriteChar(c);
+		}
+		else if(c==CR || i==len) break;
+	}
+}
+ void printStr7(char *str){       //Print String
+        while(*str){
+            UART7_WriteChar(*str);
+            str++;
+        }
+    }
