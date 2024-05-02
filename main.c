ba01c554777713de6bda5d062499d549efd0113d
Abdallah Mohamed <167894340+Abdallah-211@users.noreply.github.com>
2024-05-02 20:24:15 +0300

    Update Main code

--- main.c
+++ main.c
@@ -5,35 +5,55 @@
 #include "Systick.h"
 #include <stdint.h>
 #include <math.h>
+#include "GPS.h"
+#include "string.h"
+#include <stdio.h>
 
-
-
-float distance;
+extern double lat[100];
+extern double lon[100];
+extern char j;
 unsigned char button_in1;
 unsigned char button_in2;
-float TOTAL_distance=0;
-float total_distance =0;
+unsigned char check;
+double Total_distance =0;
+double Dis =0;
+//char ary[20] = {0}; 							//for testing
+
 int main(){
-RGB_INIT();
-SW_INITI();
-while(1){	
-button_in1 = SW1_Input();
-button_in2 = SW2_Input();
-//calculating the distance
-//passing the values from gps using UART
-//call to gps functions and store coordinates in array 
-//    where lat2 and long2 are elements A[i] and lat 1 and long 1 are elements A[i-1]
-//distance = calcdistance (1,1,11,1);
-//total_distance += distance;
-if ((button_in1 != 0x10 || total_distance == 100)){ //if sw1 is pressed then turn on red light and stop taking inputs from gps
-LEDs_output (RED);
-//stop taking new coordinates
-//array into eeprom	
-}
-if ((button_in2 != 0x01 )){ //if sw2 is pressed then clear
-LEDs_Clear();
-}
-}
-}
-// to calculate the distance you will need to call the function [calcdistance (float long1 , float lat1 , float long2 , float lat2)]
-// long1,long2,lat1,lat2 must be written in this way (ex. 4717.112671 --> 47 Degrees, 17.112671 Minutes)
+	
+		RGB_INIT();
+		UART0_Init();
+		UART1_Init();
+		SW_INITI();
+	
+while(1){
+	
+		//printStr("Enter:\n");					//for testing
+
+		button_in1 = SW1_Input();
+		button_in2 = SW2_Input();
+		GPS_format();
+	
+		if(j!=0)																												//don't calculate distance at first point
+			{
+			Dis = calcdistance(lon[j],lat[j],lon[j-1],lat[j-1]);
+			Total_distance += Dis;
+			//printStr0("Made sure of calcultaing distance \n"); 
+			}
+			j++ ; 
+
+			//printStr0("Made sure of increment \n");
+			//sprintf(ary,"Total Distance = %f \n",Total_distance);				//for testing
+			//printStr0(ary); 
+ 
+
+		if ((button_in1 != 0x10 || Total_distance >= 100)){ //if sw1 is pressed then turn on red light and stop taking inputs from gps
+			LEDs_output(RED);
+			//stop taking new coordinates
+			//array into eeprom
+			}
+		if ((button_in2 != 0x01 )){ //if sw2 is pressed then clear
+			LEDs_Clear();
+			}
+			}
+			}
