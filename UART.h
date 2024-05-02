de2448b3523cf64fe77f25416cc50f6a561094f2
Ziadd180 <168010052+Ziadd180@users.noreply.github.com>
2024-05-02 21:49:50 +0300

    Add files via upload

---
+++ UART.h
@@ -0,0 +1,45 @@
+//UART0
+void UART0_Init(void);
+
+void UART0_WriteChar(unsigned char ch);
+
+unsigned char UART0_ReadChar(void);
+
+void UART1_ReadString(char *str , int len);
+
+void printStr0(char *str);
+
+//UART1
+void UART1_Init(void);
+
+void UART1_WriteChar(unsigned char ch);
+
+unsigned char UART1_ReadChar(void);
+
+void UART1_ReadString(char *str , int len);
+
+void printStr1(char *str);
+
+
+//UART5
+void UART5_Init(void);
+
+void UART5_WriteChar(unsigned char ch);
+
+unsigned char UART5_ReadChar(void);
+
+void UART5_ReadString(char *str , int len);
+
+void printStr5(char *str);
+
+
+//UART7
+void UART7_Init(void);
+
+void UART7_WriteChar(unsigned char ch);
+
+unsigned char UART7_ReadChar(void);
+
+void UART7_ReadString(char *str , int len);
+
+void printStr7(char *str);
