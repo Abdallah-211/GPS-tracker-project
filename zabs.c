30b31d200701a3d7e0dd695fc19e9a12a6a8b050
emad <me2372003@gmail.com>
2024-05-15 20:41:03 +0300

    zabs

---
+++ zabs.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+int k=0;
+char temp [50]; // Temporary buffer to hold each float as a string
+char lat_final[2100] = ""; // Assuming a maximum length for the character array
+
+
+void ftos(float latitude[] , float longitude[], char Co_final[], int size)
+{
+	for ( k = 0; k < size; k++) {
+		
+        sprintf(temp, "%.6f", latitude[k]); // Convert float to string with 7 decimal places
+        strcat(Co_final, temp); // Concatenate the string representation
+        if (k < size ) {
+        strcat(Co_final, ","); // Add comma and space if not the last element
+        }
+    }
+	
+    for ( k = 0; k < size; k++) {
+			
+        sprintf(temp, "%.6f", longitude[k]); // Convert float to string with 7 decimal places
+        strcat(Co_final, temp); // Concatenate the string representation
+        if (k < size - 1) {
+        strcat(Co_final, ","); // Add comma and space if not the last element
+        }
+    }
+		
+		strcat(Co_final, "\n");
+		
+}
+
+
