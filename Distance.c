79bca43bbb6d8965f4f45f3031a34a960591088b
ZyadYasser0 <168011158+ZyadYasser0@users.noreply.github.com>
2024-05-16 23:00:22 +0300

    Update Distance.c

--- Distance.c
+++ Distance.c
@@ -3,28 +3,28 @@
 #include <stdio.h>
 #include "distance.h"
 #include <stdint.h>
-double distance;
-double todegree(double angle) {
-int    degree = angle / 100;                    // Extracting the degree part
-double minutes = angle - (double)degree * 100; // Extracting the minutes part
- return (degree + minutes / 60);              // Converting minutes to degrees;
+float dis;
+float todegree(float angle) {
+    int degree = angle / 100;                    // Extracting the degree part
+    float minutes = angle - (float)degree * 100; // Extracting the minutes part
+    return (degree + minutes / 60);              // Converting minutes to degrees;
 }
-double torad(double angle) {
- return angle * M_PI / 180.0;
+float torad(float angle) {
+    return angle * M_PI / 180.0;
 }
-double calcdistance (double long1 ,double lat1 ,double long2 ,double lat2){
-double long_diff;
-double lat_diff;
-double a;
-double c;
-long1 = torad (todegree( long1));
-long2 = torad (todegree( long2));
-lat2  = torad (todegree( lat2 ));
-lat1  = torad (todegree( lat1 ));
+float calcdistance (float long1 ,float lat1 ,float long2 ,float lat2){
+float long_diff;
+float lat_diff;
+float a;
+float F;
+long1 = (torad( long1));
+long2 = (torad( long2));
+lat2  = (torad( lat2 ));
+lat1  = (torad( lat1 ));
 long_diff=long1-long2;
 lat_diff= lat1-lat2;
 a= pow(sin(lat_diff/2),2) + cos(lat1) * cos(lat2) * pow(sin(long_diff/2),2);
-c = 2*atan2(sqrt(a), sqrt(1.0 - a));
-distance = Earth_Radius*c;
-return (distance);
+F = 2*atan2(sqrt(a), sqrt(1.0 - a));
+dis = 6378100*F;
+return (dis);
 }
