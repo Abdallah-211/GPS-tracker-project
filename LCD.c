a05c544ca299338f28269f8fb1ebb53925337046
ziad2100360 <168010591+ziad2100360@users.noreply.github.com>
2024-05-16 22:22:03 +0300

    Update LCD.c
    completing init  lcd , adding two functions lcd cmd and lcd init

--- LCD.c
+++ LCD.c
@@ -49,6 +49,34 @@ void PORTB_Init(){
 }
 
 
+void PORTD_Init(){
+
+	SYSCTL_RCGCGPIO_R |= 0X08;
+	while((SYSCTL_PRGPIO_R &= 0X08)==0);
+	GPIO_PORTD_CR_R |=0xFF;
+	GPIO_PORTD_AMSEL_R = 0x00;
+	GPIO_PORTD_AFSEL_R = 0x00;
+	GPIO_PORTD_PCTL_R = 0x00000000;
+	GPIO_PORTD_DIR_R =0x0FF;
+	GPIO_PORTD_DEN_R =0x0FF;
+	GPIO_PORTD_DATA_R =0x00;
+}
+
+void PORTE_Init(){
+
+	SYSCTL_RCGCGPIO_R |= 0X010;
+	while((SYSCTL_PRGPIO_R &= 0X010)==0);
+	GPIO_PORTE_CR_R |=0xFF;
+	GPIO_PORTE_AMSEL_R = 0x00;
+	GPIO_PORTE_AFSEL_R = 0x00;
+	GPIO_PORTE_PCTL_R = 0x00000000;
+	GPIO_PORTE_DIR_R =0x0FF;
+	GPIO_PORTE_DEN_R =0x0FF;
+	GPIO_PORTB_DATA_R =0x00;
+}
+
+
+
 
 
 void LCD_Data(unsigned char data){	
@@ -61,6 +89,18 @@ void LCD_Data(unsigned char data){
 	GPIO_PORTD_DATA_R &= (~(1<<2));		//Turn OFF the En of LCD
 }
 
+void LCD_Cmd(unsigned char cmd){
+	Printdata(cmd);										//Pass the 8 Bit data to the datalines of LCD
+	Generic_delay(1);
+	GPIO_PORTD_DATA_R &= (~(1<<1));		//Turn OFF the RW for write operation in LCD
+	Generic_delay(1);
+	GPIO_PORTD_DATA_R &= (~(1<<0));		//Turn OFF the RS for writing to the data register of LCD
+	Generic_delay(1);
+	GPIO_PORTD_DATA_R |= (1<<2);			//Turn ON the En of LCD for enabling the clock of LCD
+	Generic_delay(10);			//Wait for sometime
+	GPIO_PORTD_DATA_R &= (~(1<<2));		//Turn OFF the En of LCD
+}
+
 
 //to print a String which is array of char we will call LCD_Data until we print all the String
 
@@ -70,6 +110,23 @@ void LCD_String(unsigned char *str , unsigned char len){
 		LCD_Data(str[i]);
 		}
 }
+void LCD_Init(void){
+	Generic_delay(1);
+		LCD_Cmd(0x38);		//Turn ON 8 Bit Mode
+	Generic_delay(1);
+	LCD_Cmd(0x38);      //making sure
+	Generic_delay(1);
+	LCD_Cmd(0x38);      //making sure again
+	Generic_delay(1);
+	LCD_Cmd(0x08);      //turn off display
+	Generic_delay(1);
+		LCD_Cmd(0x06);		//Auto increment the cursor when prints the data in current
+	Generic_delay(1);
+		LCD_Cmd(0x01);		//Clear Screen
+	Generic_delay(1);
+	LCD_Cmd(0x0C);		//Cursor OFF and display ON
+}
+
 
 int LCD(){
 	
@@ -89,6 +146,19 @@ int LCD(){
 	GPIO_PORTB_DIR_R |= 0x10 ;
 	GPIO_PORTD_DIR_R |= 0x0F ;
 	GPIO_PORTE_DIR_R |= 0x32 ;
+
+	
+	Generic_delay(10);
+	LCD_Init();
+	Generic_delay(1);
+	LCD_Cmd(0x80);				// cursor to first row and first column
+	Generic_delay(1);
+	LCD_Data('A');
+	Generic_delay(1);
+	LCD_Cmd(0xC0);
+	Generic_delay(1);
+	LCD_String( arr,5 );
+	
 		
 	while(1){
 		
